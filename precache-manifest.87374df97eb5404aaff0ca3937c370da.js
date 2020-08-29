eada0d7594d61baf6a6af204c178a48c420a9f8f
tomerk97 <tomerk1997@gmail.com>
2020-08-29 16:33:44 +0300

    Updates

---
+++ precache-manifest.87374df97eb5404aaff0ca3937c370da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6085e12931d9f63661993f71318cb74e",
+    "url": "/tomerk97/timezone/index.html"
+  },
+  {
+    "revision": "7b93ad30713b10d16846",
+    "url": "/tomerk97/timezone/static/css/2.8b2fdb78.chunk.css"
+  },
+  {
+    "revision": "625f58210ab7d91fe0ba",
+    "url": "/tomerk97/timezone/static/css/main.5e3a2d6c.chunk.css"
+  },
+  {
+    "revision": "7b93ad30713b10d16846",
+    "url": "/tomerk97/timezone/static/js/2.7b397ac0.chunk.js"
+  },
+  {
+    "revision": "91bd7efe67ec3464cfe05e424edb44bf",
+    "url": "/tomerk97/timezone/static/js/2.7b397ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625f58210ab7d91fe0ba",
+    "url": "/tomerk97/timezone/static/js/main.073245f0.chunk.js"
+  },
+  {
+    "revision": "6bf3a87f03c9783992b7",
+    "url": "/tomerk97/timezone/static/js/runtime-main.5d52d0c2.js"
+  },
+  {
+    "revision": "b8927cf9a661fdcd6367b45e9f986478",
+    "url": "/tomerk97/timezone/static/media/ShareTechMono.b8927cf9.otf"
+  },
+  {
+    "revision": "8917a8855585e4af6d1a370a1dec1125",
+    "url": "/tomerk97/timezone/static/media/Viafont.8917a885.otf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tomerk97/timezone/precache-manifest.87374df97eb5404aaff0ca3937c370da.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tomerk97/timezone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
